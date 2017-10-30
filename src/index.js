4ee04b452cb1e7bb7c298bb9463e7d1f7012e9bb
sikanderc <sikanderchowhan@gmail.com>
2017-10-30 09:04:25 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // Code goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = React.createElement('h1', {}, 'My First React Code')
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool Stuff!')
+const container = React.createElement('div', {}, [title, paragraph])
+
+ReactDOM.render(
+  container,
+  document.getElementById('global')
+)
